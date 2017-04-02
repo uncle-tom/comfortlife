01cf207ed4c0e6dc607313281708294aed8e4ca0
Vladimir Samojlov <v-moi@ya.ru>
2017-04-02 13:28:02 +0300

    edit page php

---
+++ wp-content/themes/comfortlife/innerheader.php
@@ -0,0 +1,8 @@
+<div class="contaner-fluid padding-none">
+	<div class="welcome-block" style="background-image:url(<?php echo $mainImage ?>)">
+		<div class="welcome-block-absolute"></div>
+		<div class="welcome-block-text">
+			<?php single_post_title(); ?>
+		</div>
+	</div>
+</div>
--- wp-content/themes/comfortlife/page.php
+++ wp-content/themes/comfortlife/page.php
@@ -2,45 +2,17 @@
 <?php $mainImage = wp_get_attachment_url( get_post_thumbnail_id($post_id, 'full'))
 ?>
 
-<div class="container-fluid" style="margin-top: -150px">
-	<?php include 'leftsidebar.php'; ?>
+<?php include 'innerheader.php'; ?>
 
-	<div class="col-md-6">
-		<div id="page-header">
-			<!-- PAGE TITLE : begin -->
-			<div class="page-title">
-				<h1><?php single_post_title(); ?></h1></div>
-			<!-- PAGE TITLE : end -->
-		</div>
-		<div class="mainblock">
-			<div class="photo">
-				<img src="<?php echo $mainImage ?>" alt="">
-			</div>
-			<div class="mainblock-content post">
-				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-					<?php the_content(); ?>
-				<?php endwhile; ?>
-				<?php endif; ?>
-			</div>
-			<div class="single-post-date">
-				<?php the_time('F j, Y') ?> 
-			</div>
-		</div>
-		<div class="mainblock">
-			<div class="mainblock-title">
-				<img src="http://genichesk.info/wp-content/uploads/2017/02/double-directional-arrow.png" alt="">
-				<span>Рубрики сайта</span>
-				<div class="list-categories">
-					<ul>
-						<?php wp_list_categories('title_li=&depth=1'); ?> 
-					</ul>	
-				</div>
-			</div>
-		</div>
-	</div>
+<div class="margin50"></div>
 
-	<div class="col-md-3">
-		<?php include 'rightsidebar.php'; ?>
+<div class="container">
+	<div class="content">
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+			<?php the_content(); ?>
+		<?php endwhile; ?>
+		<?php endif; ?>
 	</div>
 </div>
+
 <?php get_footer(); ?>
